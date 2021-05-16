6dfc46867ef75f4017261d069ba9696b5a874fa9
Kirill Hrynko <ki.hrynko@gmail.com>
2021-05-16 15:43:38 +0300

    tweet quote and event listener

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+// Elements of DOM
+const quoteContainerEl = document.getElementById('quote-container');
+const quoteTextEl = document.getElementById('quote');
+const authorTextEl = document.getElementById('author');
+const twitterBtnEl = document.getElementById('twitter');
+const newQuoteBtnEl = document.getElementById('new-quote');
+
+// API's Array
 let apiQuotes = [];
 
 // Show New Quote
 const newQuote = function () {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    return quote;
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorTextEl.textContent = 'Unknown';
+    } else {
+        authorTextEl.textContent = quote.author;
+    }
+
+    // Check Quote length to determine styling
+    if (quote.text.length > 80) {
+        quoteTextEl.classList.add('long-quote');
+    } else {
+        quoteTextEl.classList.remove('long-quote');
+    }
+
+    quoteTextEl.textContent = quote.text;
 };
 
 // Get Quotes From API
@@ -18,6 +41,16 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteTextEl.textContent} - ${authorTextEl.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Tweet Quote Event Listener
+newQuoteBtnEl.addEventListener('click', newQuote);
+twitterBtnEl.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
